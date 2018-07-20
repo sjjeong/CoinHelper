27a688092b42a99ea4fb4f8b032766bc6b7146f8
SeokJun Jeong <sjjeong1225@gmail.com>
2018-07-20 19:49:44 +0900

    koin 의존성 추가

---
+++ app/src/main/java/com/googry/coinhelper/CoinHelperApplication.kt
@@ -0,0 +1,11 @@
+package com.googry.coinhelper
+
+import android.app.Application
+import org.koin.android.ext.android.startKoin
+
+class CoinHelperApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
